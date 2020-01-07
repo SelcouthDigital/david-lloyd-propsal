4a2c759b09ad88ee8c85778b210cd59063faf827
w8ski <v.simon.wierzchowski@loyalty.virgin.com>
2020-01-07 10:20:06 +0000

    theme added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  host: process.env.CONTENTFUL_HOST
+  host: process.env.CONTENTFUL_HOST,
 }
 
 const { spaceId, accessToken } = contentfulConfig
@@ -29,6 +29,14 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
-    }
+    },
+    {
+      resolve: '@narative/gatsby-theme-novela',
+      options: {
+        sources: {
+          contentful: true,
+        },
+      },
+    },
   ],
 }
